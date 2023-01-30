589dd9efad154273ac41c24a92e271797ccf4b7b
Lucas Silva Ennes <lucassilvaennes@gmail.com>
2023-01-30 01:55:02 -0300

    feat: create table to show registered spendings

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
-import { Button, Form, Layout } from "antd";
+import { Button, Form, FormProps, Layout, Table, TableProps } from "antd";
+import { useState } from "react";
 import "./App.css";
 import { Category } from "./components/Category";
-import { DateOfSpending } from "./components/DateOfSpending";
+import { DateField } from "./components/DateField";
 import { Description } from "./components/Description";
-import { SpendingType } from "./components/SpendingType";
+import { Type } from "./components/Type";
 import { Value } from "./components/Value";
+import { Dayjs } from "dayjs";
+import { DATE_FORMAT } from "./constants";
 
 const { Sider, Content } = Layout;
 
@@ -15,24 +18,56 @@ const SiderStyle: React.CSSProperties = {
   padding: "10px",
 };
 
+type Spending = {
+  category: string;
+  type: string;
+  description: string;
+  dateField: Dayjs;
+  value: number;
+};
+
+const columns: TableProps<Spending>["columns"] = [
+  { title: "Categoria", dataIndex: "category", key: "category" },
+  { title: "Tipo", dataIndex: "type", key: "type" },
+  { title: "Descrição", dataIndex: "description", key: "description" },
+  {
+    title: "Data",
+    dataIndex: "date",
+    key: "date",
+    render(value: Dayjs, record, index) {
+      return value.format(DATE_FORMAT);
+    },
+  },
+  { title: "Valor", dataIndex: "value", key: "value" },
+];
+
 function App() {
-  const [form] = Form.useForm();
+  const [form] = Form.useForm<Spending>();
+
+  const [spendings, setSpendings] = useState<Spending[]>([]);
+
+  const handleFinish: FormProps["onFinish"] = (values) => {
+    setSpendings((v) => [...v, values]);
+  };
+
   return (
     <div className="App">
       <Layout>
         <Sider style={SiderStyle} width={450}>
-          <Form form={form} onFinish={console.log}>
+          <Form form={form} onFinish={handleFinish}>
             <Category />
-            <SpendingType />
+            <Type />
             <Description />
-            <DateOfSpending />
+            <DateField />
             <Value />
             <Button type="primary" htmlType="submit">
               Salvar
             </Button>
           </Form>
         </Sider>
-        <Content></Content>
+        <Content>
+          <Table<Spending> columns={columns} dataSource={spendings} />
+        </Content>
       </Layout>
     </div>
   );
